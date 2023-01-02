086daafae1a775f7b0e80edcca41786bd29bb151
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-02 02:02:26 +0100

    Add files via upload

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,1 +1,60 @@
+import React from 'react';
+import { useState } from 'react';
+import Filter from '../Filter/Filter';
+import MovieCard from '../MovieCard/MovieCard';
 
+const MovieList = () => {
+
+    const [text, setText] = useState('')
+    const [rate, setRate] = useState('')
+    const [films, setFilms] = useState([
+        {
+            posterUrl: "https://i.pinimg.com/originals/aa/15/42/aa15422cb191838cd1b4737c7325d1fc.jpg",
+            title: "Vikings",
+            description: "This gritty drama charts the exploits of Viking hero Ragnar Lothbrok as he extends the Norse reach by challenging an unfit...",
+            rate: 2
+        },
+        
+        {
+            posterUrl: "http://img.over-blog-kiwi.com/0/95/30/84/20191024/ob_7e7b43_1571909767-action-dossier-de-presse-na.jpg",
+            title: "Narcos",
+            description: "The true story of Colombia's infamously violent and powerful drug cartels fuels this gritty gangster drama series.",
+            rate: 1
+        },
+        {
+            posterUrl: "https://i.pinimg.com/originals/e4/00/1e/e4001e0103f9a2270e8634a8cc566065.jpg",
+            title: "La Casa de Papel",
+            description: "Eight thieves take hostages and lock themselves in the Royal Mint of Spain. Starring:Úrsula Corberó, Álvaro Morte, Itziar Ituño",
+            rate: 4
+        },
+        {
+            posterUrl: "https://m.media-amazon.com/images/M/MV5BYTRiNDQwYzAtMzVlZS00NTI5LWJjYjUtMzkwNTUzMWMxZTllXkEyXkFqcGdeQXVyNDIzMzcwNjc@._V1_.jpg",
+            title: "Game of Thrones",
+            description: "Nine noble families struggle with each other for control of the fairy land of Westeros, while an ancient enemy awaits .",
+            rate: 3
+        },
+
+    ]);
+
+
+   
+    const filtertext = (text) => {
+        setText(text)
+    }
+    const filterrate = (rate) => {
+        setRate(rate)
+    }
+    return (
+        <div>
+            <Filter filtertext={filtertext} filterrate={filterrate} />
+            <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: "wrap" }} className="adc">
+                {films.filter((el) => (el.rate.toString() >= rate && el.title.toLowerCase().includes(text.toLowerCase()) )).map((element) =>
+                    <MovieCard movie={element} />
+                )}
+            </div>
+            
+        </div>
+
+    )
+};
+export default MovieList
