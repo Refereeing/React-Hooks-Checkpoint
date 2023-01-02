723fe003f53b897369474761e2901177ed504583
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-02 01:55:44 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
 
+//import ReactStars from 'react-stars';
+
+import './App.css';
+import React from 'react';
+import MovieList from "./Components/MovieList/MovieList";
+
+
+function App() {
+  return (
+
+    <div className="App" style ={{background :'white'}}>
+      <MovieList/>
+    </div>
+  );
+}
+
+export default App;
